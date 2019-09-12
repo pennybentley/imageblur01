a90991f9882d6d9b993f4df333db235683b6bc5f
penny bentley <penny.bentley@cox.net>
2019-09-12 00:18:20 +0000

    Image build complete

--- app/controllers/imageblurs_controller.rb
+++ app/controllers/imageblurs_controller.rb
@@ -2,3 +2,28 @@ class ImageblursController < ApplicationController
   def index
   end
 end
+class Image
+
+  def initialize(fill_image)
+    @image_before=fill_image
+  end
+
+ def output_image
+     @image_before.each do |arry|
+      arry.each do |cell|
+        putc cell.inspect
+      end
+      puts "\n"
+    end
+ end
+ 
+end
+
+I = Image.new([
+      [0, 0, 1, 0],
+      [0, 0, 0, 0],      
+      [0, 0, 0, 0],      
+      [0, 0, 0, 0],
+      ])
+
+I.output_image
